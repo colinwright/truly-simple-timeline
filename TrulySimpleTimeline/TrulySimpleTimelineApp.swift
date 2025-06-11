1a0ebdd9bdf8fc221eb6aac0d5fe22a74466face
Colin Wright <colingwright@gmail.com>
2025-06-11 12:37:09 -0500

    Initial Commit

---
+++ TrulySimpleTimeline/TrulySimpleTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrulySimpleTimelineApp.swift
+//  TrulySimpleTimeline
+//
+//  Created by Colin Wright on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TrulySimpleTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
